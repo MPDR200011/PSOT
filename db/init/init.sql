4de1b87d8e060201369e3d7d44600124c5f4c852
Moisés Rocha <moisesrocha99@gmail.com>
2021-12-21 11:14:25 +0000

    Finished db setup

---
+++ db/init/init.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS Scan CASCADE;
+DROP TABLE IF EXISTS Place CASCADE;
+DROP TABLE IF EXISTS Occupancy CASCADE;
+DROP TABLE IF EXISTS AccessPoint CASCADE;
+DROP TABLE IF EXISTS Client CASCADE;
+
+CREATE TABLE Place (
+	id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    capacity integer NOT NULL
+);
+
+CREATE TABLE Scan (
+	id SERIAL PRIMARY KEY,
+    scanTime timestamp,
+    placeId SERIAL REFERENCES Place ("id") ON DELETE CASCADE
+);
+
+CREATE TABLE Occupancy (
+	id SERIAL PRIMARY KEY,
+    time timestamp NOT NULL,
+    occupancyLevel real NOT NULL,
+    placeId SERIAL REFERENCES Place ("id") ON DELETE CASCADE
+);
+
+CREATE TABLE AccessPoint (
+	id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    macAddress varchar(255) NOT NULL,
+    numConnectClients integer NOT NULL,
+    scanId SERIAL REFERENCES Scan ("id") ON DELETE CASCADE
+);
+
+CREATE TABLE Client (
+	id SERIAL PRIMARY KEY,
+    macAddress varchar(255) NOT NULL,
+    scanId SERIAL REFERENCES Scan ("id") ON DELETE CASCADE,
+    accessPointId SERIAL REFERENCES AccessPoint ("id") ON DELETE CASCADE
+);
