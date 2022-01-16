b36d6f9ad69ab1f4c683205d498ca5f1d76f140e
Moisés Rocha <moisesrocha99@gmail.com>
2022-01-16 19:26:12 +0000

    create message queue

--- aggregator/src/aggregator.py
+++ aggregator/src/aggregator.py
@@ -5,6 +5,8 @@ from collections import Counter
 import sched
 import time
 import datetime
+import paho.mqtt.client as mqtt
+import json
 
 
 def getScanAccessPoints(db, scanId):
@@ -68,126 +70,134 @@ def groupBy(objList, key):
     return res
 
 
-def calculateOccupancies():
-    # dbDialect = os.environ.get("DB_DIALECT") or "postgresql"
-    dbUser = os.environ.get("DB_USER") or "jeronimo"
-    dbPassword = os.environ.get("DB_PASSWORD") or "password123"
-    dbHost = os.environ.get("DB_HOST") or "psot_db"
-    dbPort = os.environ.get("DB_PORT") or 5432
-    dbName = os.environ.get("DB_NAME") or "psot_info"
+def calculateOccupancies(mqtt_client):
+    try:
+        # dbDialect = os.environ.get("DB_DIALECT") or "postgresql"
+        dbUser = os.environ.get("DB_USER") or "jeronimo"
+        dbPassword = os.environ.get("DB_PASSWORD") or "password123"
+        dbHost = os.environ.get("DB_HOST") or "psot_db"
+        dbPort = os.environ.get("DB_PORT") or 5432
+        dbName = os.environ.get("DB_NAME") or "psot_info"
 
-    print("Connecting...")
-    db = pg.connect(
-        dbname=dbName, host=dbHost, port=dbPort, user=dbUser, passwd=dbPassword
-    )
+        print("Connecting...")
+        db = pg.connect(
+            dbname=dbName, host=dbHost, port=dbPort, user=dbUser, passwd=dbPassword
+        )
 
-    print("Removing old scans...")
-    invalidateOldScans(db)
+        print("Removing old scans...")
+        invalidateOldScans(db)
 
-    print("Calculating occupancies...")
-    allClients = getAllClients(db)
-    clientsByAP = groupBy(allClients, 'access_point_id')
-    print(f"{len(allClients)} clients, devided into {len(clientsByAP)} APs")
+        print("Calculating occupancies...")
+        allClients = getAllClients(db)
+        clientsByAP = groupBy(allClients, 'access_point_id')
+        print(f"{len(allClients)} clients, devided into {len(clientsByAP)} APs")
 
-    allAccessPoints = getAllAccessPoints(db)
-    for accessPoint in allAccessPoints:
-        if accessPoint['id'] in clientsByAP:
-            accessPoint['clients'] = clientsByAP[accessPoint['id']]
-        else:
-            accessPoint['clients'] = []
+        allAccessPoints = getAllAccessPoints(db)
+        for accessPoint in allAccessPoints:
+            if accessPoint['id'] in clientsByAP:
+                accessPoint['clients'] = clientsByAP[accessPoint['id']]
+            else:
+                accessPoint['clients'] = []
 
-    accessPointsByScan = groupBy(allAccessPoints, 'scan_id')
+        accessPointsByScan = groupBy(allAccessPoints, 'scan_id')
 
-    print(f"{len(allAccessPoints)} clients, divided into \
-    {len(accessPointsByScan)} scans")
+        print(f"{len(allAccessPoints)} clients, divided into \
+        {len(accessPointsByScan)} scans")
 
-    accessPointWhiteLists = groupBy(getAllWhiteLists(db), 'place_id')
-    scans = getScans(db)
-    for scan in scans:
-        accessPoints = []
-        if scan['id'] in accessPointsByScan:
-            accessPoints = accessPointsByScan[scan['id']]
-            if scan['place_id'] in accessPointWhiteLists:
-                whiteList = set(map(
-                    lambda x: x['name'],
-                    accessPointWhiteLists[scan['place_id']]
-                ))
+        accessPointWhiteLists = groupBy(getAllWhiteLists(db), 'place_id')
+        scans = getScans(db)
+        for scan in scans:
+            accessPoints = []
+            if scan['id'] in accessPointsByScan:
+                accessPoints = accessPointsByScan[scan['id']]
+                if scan['place_id'] in accessPointWhiteLists:
+                    whiteList = set(map(
+                        lambda x: x['name'],
+                        accessPointWhiteLists[scan['place_id']]
+                    ))
 
-                filtered = filter(
-                    lambda ap: ap['name'] in whiteList,
-                    accessPoints
-                )
-                accessPoints = list(filtered)
+                    filtered = filter(
+                        lambda ap: ap['name'] in whiteList,
+                        accessPoints
+                    )
+                    accessPoints = list(filtered)
 
-        scan['access_points'] = accessPoints
+            scan['access_points'] = accessPoints
 
-    occupancies = {}
-    scansByPlaces = groupBy(scans, 'place_id')
-    places = groupBy(getPlaces(db), 'id')
-    for (placeId, placeScans) in scansByPlaces.items():
-        occupancies[placeId] = {
-            'percentage': 0,
-            'confirmed_number': 0
-        }
+        occupancies = {}
+        scansByPlaces = groupBy(scans, 'place_id')
+        places = groupBy(getPlaces(db), 'id')
+        for (placeId, placeScans) in scansByPlaces.items():
+            occupancies[placeId] = {
+                'percentage': 0,
+                'confirmed_number': 0
+            }
 
-        # convert time stamp strings to unix timestamps
-        for scan in placeScans:
-            scanTime = time.mktime(datetime.datetime.strptime(
-                    scan['scan_time'], "%Y-%m-%d %H:%M:%S").timetuple())
-            print(f"times: {scanTime}")
-            scan['time'] = scanTime
+            # convert time stamp strings to unix timestamps
+            for scan in placeScans:
+                scanTime = time.mktime(datetime.datetime.strptime(
+                        scan['scan_time'], "%Y-%m-%d %H:%M:%S").timetuple())
+                print(f"times: {scanTime}")
+                scan['time'] = scanTime
 
-        # sort scans, most recent first
-        placeScans.sort(key=lambda s: s['time'], reverse=True)
+            # sort scans, most recent first
+            placeScans.sort(key=lambda s: s['time'], reverse=True)
 
-        partitionTimeSpan = 10
-        initTime = placeScans[0]['time']
-        currentTime = initTime
-        scanPartitions = [set()]
-        currentPartition = scanPartitions[0]
+            partitionTimeSpan = 10
+            initTime = placeScans[0]['time']
+            currentTime = initTime
+            scanPartitions = [set()]
+            currentPartition = scanPartitions[0]
 
-        # partition scans
-        for scan in placeScans:
-            while((currentTime - scan['time']) > partitionTimeSpan):
-                if len(currentPartition) > 0:
-                    # no need to create new partition if
-                    # the current one is empty
-                    newPartition = set()
-                    scanPartitions.append(newPartition)
-                    currentPartition = newPartition
-                currentTime -= partitionTimeSpan
+            # partition scans
+            for scan in placeScans:
+                while((currentTime - scan['time']) > partitionTimeSpan):
+                    if len(currentPartition) > 0:
+                        # no need to create new partition if
+                        # the current one is empty
+                        newPartition = set()
+                        scanPartitions.append(newPartition)
+                        currentPartition = newPartition
+                    currentTime -= partitionTimeSpan
 
-            for ap in scan['access_points']:
-                for client in ap['clients']:
-                    currentPartition.add(client['mac_address'])
+                for ap in scan['access_points']:
+                    for client in ap['clients']:
+                        currentPartition.add(client['mac_address'])
 
-        print(scanPartitions)
+            print(scanPartitions)
 
-        # count in how many partitions each mac address appears
-        counts = Counter()
-        for partition in scanPartitions:
-            counts.update(partition)
-        print(counts)
+            # count in how many partitions each mac address appears
+            counts = Counter()
+            for partition in scanPartitions:
+                counts.update(partition)
+            print(counts)
 
-        # chooses mac adresses that appear in
-        # a certain amount of different partitions
-        threshold = 2
-        confirmed = set(x for x, count in counts.items() if count >= threshold)
+            # chooses mac adresses that appear in
+            # a certain amount of different partitions
+            threshold = 2
+            confirmed = set(
+                x for x, count in counts.items() if count >= threshold)
 
-        place = places[placeId][0]
-        occupancies[placeId] = {
-            'percentage': (len(confirmed) * place['callibration_constant'])
-            / place['capacity'],
-            'confirmed_number': len(confirmed)
-        }
+            place = places[placeId][0]
+            occupancies[placeId] = {
+                'percentage': (len(confirmed) * place['callibration_constant'])
+                / place['capacity'],
+                'confirmed_number': len(confirmed)
+            }
 
-        print(f"Place {place['name']}, \
-        calculated {len(confirmed)} people")
+            print(f"Place {place['name']}, \
+            calculated {len(confirmed)} people")
 
-    if len(occupancies) > 0:
-        addOccupancies(db, occupancies)
+        if len(occupancies) > 0:
+            addOccupancies(db, occupancies)
+        db.close()
 
-    db.close()
+        print("Publishing to MQTT...")
+        mqtt_client.publish('occupancy_notifications', json.dumps(occupancies))
+        print("Done")
+
+    except Exception as e:
+        print(f"Error aggregating: {e}")
 
 
 def periodic(scheduler, interval, action, actionargs=()):
@@ -196,11 +206,24 @@ def periodic(scheduler, interval, action, actionargs=()):
     action(*actionargs)
 
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("MQTT Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+
+
 def main():
     load_dotenv()
 
+    client = mqtt.Client()
+    client.on_connect = on_connect
+
+    client.connect("psot_notifications", 1883, 60)
+    client.loop_start()
+
     scheduler = sched.scheduler(time.time, time.sleep)
-    periodic(scheduler, 60, calculateOccupancies)
+    periodic(scheduler, 60, calculateOccupancies, [client])
     scheduler.run()
 
 
