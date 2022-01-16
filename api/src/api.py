60f9bb3c880059ec3ae497be88e7fae11463d779
Moisés Rocha <moisesrocha99@gmail.com>
2022-01-16 16:31:02 +0000

    create api

---
+++ api/src/api.py
@@ -0,0 +1,64 @@
+import pg
+import os
+from flask import Flask
+from flask_cors import CORS
+
+"""
+    SELECT tbl.*
+FROM place_occupancy tbl
+  right JOIN
+  (
+    SELECT id, max(time) as maxTime
+    FROM place_occupancy
+    GROUP BY id
+  ) tbl1
+  ON tbl1.id = tbl.id
+WHERE tbl1.maxTime = tbl.time;
+"""
+
+dbUser = os.environ.get("DB_USER") or "jeronimo"
+dbPassword = os.environ.get("DB_PASSWORD") or "password123"
+dbHost = os.environ.get("DB_HOST") or "psot_db"
+dbPort = os.environ.get("DB_PORT") or 5432
+dbName = os.environ.get("DB_NAME") or "psot_info"
+
+
+def groupBy(objList, key):
+    res = {}
+
+    for obj in objList:
+        if obj[key] not in res:
+            res[obj[key]] = []
+
+        res[obj[key]].append(obj)
+
+    return res
+
+
+app = Flask(__name__)
+CORS(app)
+
+db = pg.connect(
+    dbname=dbName, host=dbHost, port=dbPort, user=dbUser, passwd=dbPassword
+)
+
+
+def queryPlaces(db):
+    return db.query("select * from place").dictresult()
+
+
+def queryPlacesOccupancies(db):
+    return db.query("select * from recent_occupancies").dictresult()
+
+
+@app.route("/places")
+def getPlaces():
+    return {'places': queryPlaces(db)}
+
+
+@app.route("/occupancies")
+def getOccupancies():
+    occupancies = groupBy(queryPlacesOccupancies(db), 'id')
+    for placeId in occupancies.keys():
+        occupancies[placeId] = occupancies[placeId][0]
+    return occupancies
